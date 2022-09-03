1d4dc133655a99af6bcebd8b1e10b8b2d0b02d15
CharlotteHan <lightenvee@gmail.com>
2022-09-03 16:21:13 +1000

    Login page and superuser created

--- capstoneEngine/capstoneEngine/settings.py
+++ capstoneEngine/capstoneEngine/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'capstoneEngine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+		'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/ACT'
 
 USE_I18N = True
 
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#specify route for login
+LOGIN_REDIRECT_URL = "/"
