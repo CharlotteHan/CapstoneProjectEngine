b875bdb2950b33d6294f41f1528072fddba2f960
CharlotteHan <lightenvee@gmail.com>
2022-10-12 00:47:25 +1100

    add style for project list and eoi submission

--- capstoneEngine/projects/forms.py
+++ capstoneEngine/projects/forms.py
@@ -9,9 +9,9 @@ class ProjectForm(forms.ModelForm):
         fields = ['title','description','in_scope','out_scope','team_size','duration']
 
 class EOIForm(forms.ModelForm):
-    group_member1 = forms.CharField(max_length=50, label='Group Member ID')
-    group_member2 = forms.CharField(max_length=50, label='Group Member ID')
-    group_member3 = forms.CharField(max_length=50, label='Group Member ID', required=False)
+    group_member1 = forms.CharField(max_length=50, label='Group Member ID', widget=forms.Textarea(attrs={'class': 'eleForI Huans'}))
+    group_member2 = forms.CharField(max_length=50, label='Group Member ID', widget=forms.Textarea(attrs={'class': 'eleForI Huans'}))
+    group_member3 = forms.CharField(max_length=50, label='Group Member ID', widget=forms.Textarea(attrs={'class': 'eleForI Huans'}), required=False)
     class Meta:
         model = Group
         fields = ['choice1','choice2','choice3']
@@ -19,6 +19,11 @@ class EOIForm(forms.ModelForm):
             'choice1': _('Choice1'),
             'choice2': _('Choice2'),
             'choice3': _('Choice3'),
+        }
+        widgets = {
+            'choice1': forms.Select(attrs={'class': 'eleForI Huans'}),
+            'choice2': forms.Select(attrs={'class': 'eleForI Huans'}),
+            'choice3': forms.Select(attrs={'class': 'eleForI Huans'}),
         }
         
 class AllocateForm(forms.ModelForm):
